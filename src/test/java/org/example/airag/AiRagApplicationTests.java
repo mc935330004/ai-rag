c87a6e4a0b0c9600ea9f9f3e5051a4fee56ed0f7
孟闯 <mengchuang_java@163.com>
2026-06-18 13:21:49 +0800

    first commit

---
+++ src/test/java/org/example/airag/AiRagApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.airag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiRagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
