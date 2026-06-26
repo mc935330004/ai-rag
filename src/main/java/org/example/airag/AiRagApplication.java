4986398bafeaf4850daaf6b995d74f821d778e6c
孟闯 <mengchuang_java@163.com>
2026-06-26 10:58:20 +0800

    提交

--- src/main/java/org/example/airag/AiRagApplication.java
+++ src/main/java/org/example/airag/AiRagApplication.java
@@ -8,6 +8,7 @@ public class AiRagApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AiRagApplication.class, args);
+        System.out.println("登录成功！！！");
     }
 
 }
